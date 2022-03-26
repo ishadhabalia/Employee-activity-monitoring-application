f0ed053bd9636a434d361c77682b75f5d8b8df59
mahidedhia <mahi.dedhia@somaiya.edu>
2022-03-27 00:45:32 +0530

    Deleted Unnecessary files

--- enable-venv.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd Scripts/
-activate
-cd ..
--- run_schtask.bat
+++
@@ -1,4 +0,0 @@
-
-            :: this batch file is used to run the script.py
-            py "D:\mahi\Projects\Employee-Tracking-Website\Employee-activity-monitoring-application\script.py" 
-        
