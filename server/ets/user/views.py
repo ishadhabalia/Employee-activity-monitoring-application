23680a227ca860b1da0f13a806280a3786d85381
ishadhabalia <isha.dhabalia@somaiya.edu>
2022-05-16 21:26:55 +0530

    Updated Base

--- server/ets/user/views.py
+++ server/ets/user/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def login(request):
-    return render(request,'user/login.html')
+    return render(request,'user/login.html',{'nbar': 'login','role':'manager'})
 
 def dispProfile(request):
-    return render(request,'user/profile.html')
+    return render(request,'user/profile.html',{'nbar': 'home','profile':'manager'})
