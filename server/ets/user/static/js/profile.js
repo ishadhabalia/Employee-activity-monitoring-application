bdb08df182d7fb42129962dca0f704cfd5d62f59
mahidedhia <mahi.dedhia@somaiya.edu>
2022-05-16 21:43:38 +0530

    updating changes in user app and dashboard app

--- server/ets/user/static/js/profile.js
+++ server/ets/user/static/js/profile.js
@@ -3,18 +3,32 @@
 // emptyDiv.css("height", headerHeight);
 
 // var headerHeight = document.getElementById('header').style.height;
-var rect = document.getElementById('header').getBoundingClientRect();
-var headerHeight = rect.height;
-var emptyDiv = document.getElementById('empty');
-emptyDiv.style.marginTop=String(headerHeight)+"px";
 
-window.onresize = movePageTitleDiv;
+window.onload = function(){
+    var rect = document.getElementById('header').getBoundingClientRect();
+    var headerHeight = rect.height;
+    var emptyDiv = document.getElementById('empty');
+    emptyDiv.style.marginTop=String(headerHeight)+"px";
 
-function movePageTitleDiv(){
+    moveElements();
+}
+
+window.onresize = moveElements;
+
+function moveElements(){
     var rect = document.getElementById('sidebar').getBoundingClientRect();
     var width = rect.width;
-    var left = rect.left;
-    var move = width + left;
+
+    var windowWidth = document.body.clientWidth;
+    if(windowWidth<1200){
+        var move = 20;
+    }
+    else{
+        var move = width + 20;
+    }
+
     var pageTitle = document.getElementById('pagetitle');
     pageTitle.style.marginLeft=String(move)+"px";
+    var profilesection = document.getElementById('profile-section');
+    profilesection.style.marginLeft=String(move)+"px";
 }
