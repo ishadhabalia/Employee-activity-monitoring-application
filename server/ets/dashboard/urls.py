0fbecb95afb8c44aa78a78431cc9cfd0669a74d8
ishadhabalia <isha.dhabalia@somaiya.edu>
2022-05-16 20:28:09 +0530

    Updated dashboard ui

--- server/ets/dashboard/urls.py
+++ server/ets/dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 # path('dashboard/<str:empid>/', views.dashboard, name = 'DashBoard'),
 path('', views.view_dashboard, name = 'ViewDashBoard'),
+path('teams/', views.view_teams, name = 'Teams'),
 ]
--- server/ets/dashboard/views.py
+++ server/ets/dashboard/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def view_dashboard(request):
-     return render(request,'dashboard/dashboard_home.html')
+     return render(request,'dashboard/dashboard_home.html',{'nbar': 'home','role':'manager'})
+
+def view_teams(request):
+     return render(request,'dashboard/teams.html',{'nbar': 'teams','role':'manager'})
